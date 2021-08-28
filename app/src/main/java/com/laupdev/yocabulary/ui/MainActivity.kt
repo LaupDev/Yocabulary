6ed8aee2886d281cbd4c58d41e8e84733961a946
Pavlo Penzelyk <p.penzelik@gmail.com>
2021-08-29 01:58:54 +0300

    Added bottom navigation for main app navigation

--- app/src/main/java/com/laupdev/yocabulary/ui/MainActivity.kt
+++ app/src/main/java/com/laupdev/yocabulary/ui/MainActivity.kt
@@ -3,18 +3,47 @@ package com.laupdev.yocabulary.ui
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation.LabelVisibilityMode
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationBarView.LABEL_VISIBILITY_SELECTED
 import com.laupdev.yocabulary.R
+import com.laupdev.yocabulary.databinding.ActivityMainBinding
 import com.laupdev.yocabulary.network.CheckNetwork
 
 class MainActivity : AppCompatActivity() {
 
 //    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigation: BottomNavigationView = binding.bottomNavigation
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_practice, R.id.navigation_sets, R.id.navigation_vocabulary, R.id.navigation_settings
+//            )
+//        )
+//
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigation.setupWithNavController(navController)
+        bottomNavigation.labelVisibilityMode = LABEL_VISIBILITY_SELECTED
 
 //        applicationContext.deleteDatabase("yocabulary_database")
 
